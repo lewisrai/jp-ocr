b7ebd6a7d98bbccc982493edc3df606ae7ab9726
lewisrai <144852757+lewisrai@users.noreply.github.com>
2023-09-27 13:54:56 +0100

    Removed Logging, Simplified Page

--- static/home.js
+++ static/home.js
@@ -1,72 +1,40 @@
-function startVideoStream() {
-    navigator.mediaDevices.getDisplayMedia({
-        audio: false,
-        video: {
-            width: {
-                ideal: 2560
-            },
-            height: {
-                ideal: 1600
-            },
-            framerate: {
-                max: 15
-            }
-        }
-    }).then(stream => {
-        videoStreamID.srcObject = stream;
-        console.log("INFO: Successfully Capturing Screen");
-    }).catch(console.error)
-}
-
-
 function cropAndProcessImage(x, y, width, height) {
-    console.log("INFO: Cropping User Selection");
-
     fullCanvas.getContext('2d').drawImage(videoStreamID, 0, 0);
 
     croppedCanvas.width = width;
     croppedCanvas.height = height;
     croppedCanvas.getContext('2d').drawImage(fullCanvas, x, y, width, height, 0, 0, width, height);
 
-    displayedCanvasID.getContext('2d').drawImage(croppedCanvas, 0, 0, width, height, 0, 0, displayedCanvasID.width, displayedCanvasID.height);
-
-    const dataURL = croppedCanvas.toDataURL();
-
-    console.log("INFO: Sending Cropped Screenshot For OCR");
-
     $.ajax({
         type: "POST",
         url: "/process",
         data: {
-            imageBase64: dataURL
+            imageBase64: croppedCanvas.toDataURL()
         },
         success: function(response) {
             textOutputID.innerHTML = response;
         }
-    }).done(function() {
-            console.log("INFO: AJAX Request Has Completed!");
     })
 }
 
 
 function userSelectedArea(event) {
-    if (firstTime == true) {
-        streamResolutionX = videoStreamID.videoWidth;
-        streamResolutionY = videoStreamID.videoHeight;
-        fullCanvas.width = streamResolutionX;
-        fullCanvas.height = streamResolutionY;
-        console.log("INFO: Stream Resolution: " + streamResolutionX + "x" + streamResolutionY);
-        firstTime = false;
+    if (cropState.firstTime == true) {
+        streamResolution.x = videoStreamID.videoWidth;
+        streamResolution.y = videoStreamID.videoHeight;
+        fullCanvas.width = streamResolution.x;
+        fullCanvas.height = streamResolution.y;
+        cropState.firstTime = false;
     }
 
-    if (secondSelectionClick == false) {
-        selectionStartX = event.offsetX * streamResolutionX / 640;
-        selectionStartY = event.offsetY * streamResolutionY / 400;
-        secondSelectionClick = true;
+    if (cropState.endSelection == false) {
+        selectionStart.x = event.offsetX * streamResolution.x / 960;
+        selectionStart.y = event.offsetY * streamResolution.y / 600;
+        cropState.endSelection = true;
     }
     else {
-        cropAndProcessImage(selectionStartX, selectionStartY, (event.offsetX * streamResolutionX / 640) - selectionStartX, (event.offsetY * streamResolutionY / 400) - selectionStartY);
-        secondSelectionClick = false;
+        cropAndProcessImage(selectionStart.x, selectionStart.y, (event.offsetX * streamResolution.x / 960) - selectionStart.x, (event.offsetY * streamResolution.y / 600) - selectionStart.y);
+        cropState.endSelection = false;
     }
 }
 
@@ -77,24 +45,29 @@ const fullCanvas = document.createElement('canvas');
 
 const croppedCanvas = document.createElement('canvas');
 
-const displayedCanvasID = document.getElementById('displayedCanvas');
-
 const textOutputID = document.getElementById('textOutput');
-
-startVideoStream()
 
 videoStreamID.addEventListener('click', userSelectedArea);
 
-var streamResolutionX = undefined;
+var streamResolution = {x: undefined, y: undefined};
 
-var streamResolutionY = undefined;
+var cropState = {firstTime: true, endSelection: false};
 
-var firstTime = true;
+var selectionStart = {x: undefined, y: undefined};
 
-var secondSelectionClick = false;
-
-var selectionStartX = undefined;
-
-var selectionStartY = undefined;
-
-console.log("INFO: Initialised");
+navigator.mediaDevices.getDisplayMedia({
+    audio: false,
+    video: {
+        width: {
+            ideal: 2560
+        },
+        height: {
+            ideal: 1600
+        },
+        framerate: {
+            max: 15
+        }
+    }
+}).then(stream => {
+    videoStreamID.srcObject = stream;
+}).catch(console.error);
