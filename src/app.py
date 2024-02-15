eb74bee9c83ee9b7e5f10ab140cca904ab89d93f
lewisrai <144852757+lewisrai@users.noreply.github.com>
2024-02-15 16:35:05 +0000

    Format

--- src/app.py
+++ src/app.py
@@ -10,22 +10,25 @@ from transformers import AutoFeatureExtractor, AutoTokenizer, VisionEncoderDecod
 
 class MangaOcr:
     def __init__(self, pretrained_model_path="OCRModel"):
-        self.feature_extractor = AutoFeatureExtractor.from_pretrained(pretrained_model_path)
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained(
+            pretrained_model_path
+        )
         self.model = VisionEncoderDecoderModel.from_pretrained(pretrained_model_path)
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_path)
 
-
     def __call__(self, img):
-        img = img.convert('L').convert('RGB')
+        img = img.convert("L").convert("RGB")
         img = self.feature_extractor(img, return_tensors="pt").pixel_values
         img = img.squeeze()
 
-        x = self.model.generate(img[None].to(self.model.device), max_length=300)[0].cpu()
+        x = self.model.generate(img[None].to(self.model.device), max_length=300)[
+            0
+        ].cpu()
         text = self.tokenizer.decode(x, skip_special_tokens=True)
 
-        text = ''.join(text.split())
-        text = text.replace('…', '...')
-        text = re.sub('[・.]{2,}', lambda x: (x.end() - x.start()) * '.', text)
+        text = "".join(text.split())
+        text = text.replace("…", "...")
+        text = re.sub("[・.]{2,}", lambda x: (x.end() - x.start()) * ".", text)
         text = jaconv.h2z(text, ascii=True, digit=True)
 
         return text
@@ -34,19 +37,17 @@ class MangaOcr:
 def main():
     mocr = MangaOcr()
 
-
     flask_app = Flask(__name__)
 
     @flask_app.route("/")
     def home():
         return render_template("index.html")
 
-    @flask_app.route("/api", methods=['POST'])
+    @flask_app.route("/api", methods=["POST"])
     def process():
         image_base64 = re.sub("^data:image/.+;base64,", "", request.form["imageBase64"])
         image = Image.open(BytesIO(base64.b64decode(image_base64)))
         return mocr(image)
-
 
     flask_app.run(host="0.0.0.0")
 
