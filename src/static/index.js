64e375fa90243ae070d78aac556682eed93e7d24
lewisrai <144852757+lewisrai@users.noreply.github.com>
2024-03-12 03:34:01 +0000

    Format

--- src/static/index.js
+++ src/static/index.js
@@ -1,61 +1,69 @@
 function cropAndProcessImage(x, y, width, height) {
     croppedCanvas.width = width;
     croppedCanvas.height = height;
-    croppedCanvas.getContext('2d').drawImage(videoStreamID, x, y, width, height, 0, 0, width, height);
+    croppedCanvas
+        .getContext("2d")
+        .drawImage(videoStreamID, x, y, width, height, 0, 0, width, height);
 
     $.ajax({
         type: "POST",
         url: "/api",
-        data: {
-            imageBase64: croppedCanvas.toDataURL()
-        },
-        success: function(response) {
+        data: { imageBase64: croppedCanvas.toDataURL() },
+        success: (response) => {
             textOutputID.innerHTML = response;
-        }
-    })
+        },
+    });
 }
 
-
 function userSelectedArea(event) {
-    const select = {x: event.offsetX * videoStreamID.videoWidth / 960, y: event.offsetY * videoStreamID.videoHeight / 600}
+    const select = {
+        x: (event.offsetX * videoStreamID.videoWidth) / 960,
+        y: (event.offsetY * videoStreamID.videoHeight) / 600,
+    };
 
-    if (endSelection == false) {
+    if (endSelection === false) {
         selectionStart.x = select.x;
         selectionStart.y = select.y;
         endSelection = true;
-    }
-    else {
-        cropAndProcessImage(selectionStart.x, selectionStart.y, select.x - selectionStart.x, select.y - selectionStart.y);
+    } else {
+        cropAndProcessImage(
+            selectionStart.x,
+            selectionStart.y,
+            select.x - selectionStart.x,
+            select.y - selectionStart.y,
+        );
         endSelection = false;
     }
 }
 
+const videoStreamID = document.getElementById("video-stream");
 
-const videoStreamID = document.getElementById('video-stream');
+const textOutputID = document.getElementById("text-output");
 
-const textOutputID = document.getElementById('text-output');
+const croppedCanvas = document.createElement("canvas");
 
-const croppedCanvas = document.createElement('canvas');
+const selectionStart = { x: undefined, y: undefined };
 
-const selectionStart = {x: undefined, y: undefined};
+let endSelection = false;
 
-var endSelection = false;
-
-navigator.mediaDevices.getDisplayMedia({
-    audio: false,
-    video: {
-        width: {
-            ideal: 2560
+navigator.mediaDevices
+    .getDisplayMedia({
+        audio: false,
+        video: {
+            width: {
+                ideal: 2560,
+            },
+            height: {
+                ideal: 1600,
+            },
+            framerate: {
+                max: 5,
+            },
         },
-        height: {
-            ideal: 1600
-        },
-        framerate: {
-            max: 5
-        }
-    }
-}).then(stream => {
-    videoStreamID.srcObject = stream;
-}).catch(console.error);
+    })
+    .then((stream) => {
+        videoStreamID.srcObject = stream;
+    })
+    .catch(console.error);
 
-videoStreamID.addEventListener('click', userSelectedArea);
+videoStreamID.addEventListener("click", userSelectedArea);
